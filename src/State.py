f1bc8e4d1fff6aaace7e327e743f53aa010fbe54
Andrew Dempsey <amd0331@gmail.com>
2019-05-24 15:39:01 -0400

    Add state documentation

--- src/State.py
+++ src/State.py
@@ -2,20 +2,31 @@ import numpy as np
 import sys
 
 class State():
+    """
+    @param size - tuple of image dimensions (batchsize, x, y, z)
+    @param move_range - the number of actions for directly modifying voxel
+    intensities (as defined in config/MOVE_RANGE)
+    """
     def __init__(self, size, move_range):
         self.image = np.zeros(size,dtype=np.float32)
         self.move_range = move_range
 
+    """
+    Set up initial state at the beginning of the episode
+    @param x - The image to operate on
+    """
     def reset(self, x):
         self.image = x
         size = self.image.shape
         prev_state = np.zeros((size[0],16,size[2],size[3],size[4]),dtype=np.float32)
         self.tensor = np.concatenate((self.image, prev_state), axis=1)
 
-    def set(self, x):
-        self.image = x
-        self.tensor[:,:self.image.shape[1],:,:,:] = self.image
-
+    """
+    Perform execution of an action on an image
+    @param act - a cube with the same dimension as the image composed of
+    scalar values corresponding to actions
+    @inner_state - GRU hidden state
+    """
     def step(self, act, inner_state):
         move = act.astype(np.float32)
 
